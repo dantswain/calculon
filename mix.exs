94a7f8278c6d2972c1f0669634117e577950cc08
Dan Swain <dan.t.swain@gmail.com>
2015-04-19 13:18:34 -0500

    Add httpoison

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Calculon.Mixfile do
      {:phoenix_ecto, "~> 0.3"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_live_reload, "~> 0.3"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:httpoison, "~> 0.6", only: [:dev, :test]}]
   end
 end
